1d1943b440e634ac50f801191ebcafbc3032ed1c
Pavol Humeny <pavol.humeny@gmail.com>
2025-06-16 09:54:52 +0200

    feat: Implement help and settings button component

---
+++ src/components/topPanel/HelpButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BaseIcon from '@/components/icons/BaseIcon.vue';
+</script>
+
+<template>
+  <button class="button circle-button control-button">
+    <BaseIcon name="IconQuestionMark" :size="23"/>
+  </button>
+</template>
+
+<style scoped>
+</style>
