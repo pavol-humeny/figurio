74b1ba8d9bec6fd6409c65f31a743072161ae15f
Pavol Humeny <pavol.humeny@gmail.com>
2025-06-17 07:24:39 +0200

    feat: Add icon cross

---
+++ src/components/icons/IconCross.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.625 -0.625 20 20" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x" id="X--Streamline-Feather" height="20" width="20">
+  <path d="M14.0625 4.6875 4.6875 14.0625" stroke-width="1.25"></path>
+  <path d="m4.6875 4.6875 9.375 9.375" stroke-width="1.25"></path>
+</svg>
+</template>
