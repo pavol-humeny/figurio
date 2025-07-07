f6a1710697f2fa064200d3cccc06f339cc5b83b8
Pavol Humeny <pavol.humeny@gmail.com>
2025-07-07 16:25:10 +0200

    fix: Remove tmp button from home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,10 @@
 <script setup>
 import DragAndDropArea from '@/components/editor/DragAndDropArea.vue';
 
-import { useRouter } from 'vue-router';
-const router = useRouter();
-
-const ahoj = () => {
-  if (router.currentRoute.value.name !== 'editor') {
-    router.push({ name: 'editor' })
-  }
-};
-
 </script>
 
 <template>
   <div class="home-view">
-    <button @click="ahoj">Move</button>
     <DragAndDropArea />
   </div>
 </template>
