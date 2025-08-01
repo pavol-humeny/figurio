2348805dc2d540f818f7ed9ee336d58886787f4c
Pavol Humeny <pavol.humeny@gmail.com>
2025-08-01 21:15:14 +0200

    fix: Swt deployment commit message with more informations

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,28 @@
 # Stop on errors
 set -e
 
-# Build
+# Build the project
 npm run build
 
-# Navigate to the build output directory
+# Move to the build output directory
 cd dist
 
-# Initialize a new git repository
+# Initialize a new git repo
 git init
 git add -A
-git commit -m 'New Deployment'
 
-# Push to the gh-pages branch of the repository
+# Get data from parent folder (project root)
+VERSION=$(node -p "require('../package.json').version")
+BRANCH=$(git -C .. rev-parse --abbrev-ref HEAD)
+HASH=$(git -C .. log -1 --format=%h)
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Compose commit message
+COMMIT_MSG="Deploy release v$VERSION from $BRANCH ($HASH) at $DATE"
+
+# Commit and push
+git commit -m "$COMMIT_MSG"
 git push -f git@github.com:pavol-humeny/BP_Image_editor.git master:gh-pages
 
-# Return to the previous directory
+# Go back
 cd -
