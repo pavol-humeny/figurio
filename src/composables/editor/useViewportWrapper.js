c7898acb1d2f5a97b2601a7f804d0445e0ec5c9c
Pavol Humeny <pavol.humeny@gmail.com>
2025-08-19 09:36:37 +0200

    fix: closing right panel with different zoom than 100 was moving image

--- src/composables/editor/useViewportWrapper.js
+++ src/composables/editor/useViewportWrapper.js
@@ -380,7 +380,7 @@ export function useViewportWrapper(viewportStore, imageStore, editorStore, uiSto
     if (!wrapperRef.value || !contentRef.value) return
     // Reset zoom
     const tmpZoomLevel = viewportStore.zoomLevel
-    viewportStore.resetZoom()
+    // viewportStore.resetZoom() // TODO - delete if everything works (closing right panel with different zoom than 100 was moving image)
 
     updateInitialDimensions()
     updateZoomDependentDimensions()
