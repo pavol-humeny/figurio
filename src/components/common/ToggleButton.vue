f7c13dfe311d428b859c6668d2af322f052750a9
Pavol Humeny <pavol.humeny@gmail.com>
2025-08-25 14:00:11 +0200

    fix: Change toggle button to be more intuitive

--- src/components/common/ToggleButton.vue
+++ src/components/common/ToggleButton.vue
@@ -50,7 +50,8 @@ const { isActive, toggleSwitch } = useToggleButton(props, emit)
 <template>
   <ItemTip :text="props.tip" :position="props.position">
     <div class="toggle-switch" :style="{ transform: `scale(${props.scale})` }">
-      <div class="toggle-switch-wrapper" :class="{ 'toggle-disabled': props.disabled }" @click="toggleSwitch">
+      <div class="toggle-switch-wrapper" :class="{ 'toggle-disabled': props.disabled, active: isActive }"
+        @click="toggleSwitch">
         <div class="toggle-switch-slider" :class="{ active: isActive }"></div>
       </div>
     </div>
@@ -67,13 +68,18 @@ const { isActive, toggleSwitch } = useToggleButton(props, emit)
 .toggle-switch-wrapper {
   position: relative;
   display: flex;
-  background: var(--secondary-c);
+  border: 2px solid var(--secondary-c);
   border-radius: 20px;
   padding: 5px;
   width: 80px;
   height: 40px;
   overflow: hidden;
   cursor: pointer;
+  transition: var(--default-transition);
+}
+
+.toggle-switch-wrapper.active {
+  background: var(--secondary-c);
   transition: var(--default-transition);
 }
 
@@ -84,12 +90,13 @@ const { isActive, toggleSwitch } = useToggleButton(props, emit)
 
 .toggle-switch-slider {
   position: absolute;
-  top: 5px;
-  opacity: 0.5;
+  top: 3px;
+  opacity: 0.8;
   left: 5px;
   width: calc(100% / 2 - 5px);
   height: 30px;
   background: var(--primary-c);
+  border: 2px solid var(--primary-c);
   border-radius: 15px;
   transition: var(--default-transition);
 }
