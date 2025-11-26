ecdde92732310a8e7a90e0a023fa61db76d57037
Pavol Humeny <pavol.humeny@gmail.com>
2025-11-26 10:25:12 +0100

    refactor: Remove console log

--- src/composables/common/useStepperInput.js
+++ src/composables/common/useStepperInput.js
@@ -51,7 +51,6 @@ export function useStepperInput(props, emit) {
    * Emits the updated value to the parent component
    */
   const emitChange = () => {
-    console.warn('PROP.MODELVALUE:', props.modelValue)
     emit('update:modelValue', inputValue.value)
     emit('update', inputValue.value)
   }
